8dbe13c828995178d4c4349e21f8912b661187b5
Alice <a.j.stack@sbcglobal.net>
2018-04-19 13:45:19 -0500

    working on javascript display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,31 +13,41 @@ var wordList = [
     "BATWOMAN" 
 ];
 
+// setup do display to html
 var winCount = document.getElementById("winCount");
 var word = document.getElementById("word");
 var guessCount = document.getElementById("guessCount");
+    guessCount.textcontent = 10;
 var letters = document.getElementById("letters");
+var win = wordLength;
+
+
+// Choose random # to correspond with array words
 var choice = Math.floor(Math.random() * wordList.length);
 var answer = wordList[choice];
+var wordLength = answer.length;
 
 
+// display how many letters from word chosen
+var display = [wordLength];
 
+for (var i = 0; i < answer.length; i++) {
+    display[i] = "_ ";
+
+}
+    
 // onkeyup function event
 document.onkeyup = function(event) {
-
 var userGuess = event.key;
 
 // make all keys uppercase
-//.toUpperCase()
 userGuess = userGuess.toUpperCase();
-// determine what key is pressed
-letters.textContent = event.key; 
+
+// print guess to html
+letters.textContent = userGuess; 
 
 // keep track of all letters pressed // trigger-random-unsolved week 4 day 2
 
-// computer chooses a random word form array
-
-// display how many letters from word chosen
 
 // for loop (var i=0; i<=(forever? until words run out?); i++)
     // if statement
