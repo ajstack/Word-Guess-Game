20217eec81b0560dd1f9a8c006ce307076ab4377
Alice <a.j.stack@sbcglobal.net>
2018-04-20 23:43:42 -0500

    working stuff out is hard

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,15 +14,14 @@ var wordList = [
 ];
 
 // setup do display to html
-var winCount = document.getElementById("winCount");     // How many times user has won
-var word = document.getElementById("word");             // Word chosen
-var guessCount = document.getElementById("guessCount"); // How many guesses left display
+var winCount = document.getElementById("winCount");     // display how many times user has won
+var word = document.getElementById("word");             // display word chosen
+var guessCount = document.getElementById("guessCount"); // display how many guesses left 
 var attemptsLeft = 10;                                  // Setting number of guesses
-var letters = document.getElementById("letters");       // incorrect letters already guessed
+var letters = document.getElementById("letters");       // display incorrect letters already guessed
 var display = [wordLength];                             // display _ 
 var win = wordLength;                                   // number of correct letters needed to win
-var output = "";                                        // set original output to nothing
-
+var letterBank = [];                                    // empty array for guessed letters
 
 // Display how many guesses left
 guessCount.textContent = attemptsLeft;
@@ -36,13 +35,15 @@ winCount.textContent = 0;
 var choice = Math.floor(Math.random() * wordList.length);
 var answer = wordList[choice];
 var wordLength = answer.length;
+// var guesses = answer.split('');
+
 
 
 // display how many letters from word chosen {not working}
-for (var i = 0; i < answer.length; i++) {
-    display[i] = "_ ";
-    display.textContent = word; 
-}
+//for (var i = 0; i < answer.length; i++) {
+    //display[i] = "_ ";
+    //display.textContent = word; 
+//}
 
 // onkeyup function event
 document.onkeyup = function(event) {
@@ -54,22 +55,33 @@ userGuess = userGuess.toUpperCase();
 // print guess to html
 letters.textContent = userGuess; 
 
-// keep track of all letters pressed // trigger-random-unsolved week 4 day 2 // drinklist-solved
+// keep track of all letters pressed // trigger-random-unsolved week 4 day 2 // drinklist-solved {not working}
+letterBank.push(userGuess);
 
-//for (var i = 0; i < _____ ; i++) {
-
-    //_____.appendChild(letters)
-//}
+for (var i = 0; i < letterBank.length; i++) {
+     var letterDisplay = document.createElement("p");
+     letterDisplay.textContent = letterBank[i];
+     letters.appendChild(letterDisplay);
+}
 
 // for loop (var i=0; i<=(forever? until words run out?); i++)
     // if statement
         // if word has been guessed  
         //winCount++ 
-        // else if #guessCount=0, restart
+        // else if guessCount=0, restart
         //else
             // if statement
                 // if letter is in word, display in #word
                 // else letter is not in word, display in letters, guessCount -1
 
+
+
+
+
+
+
+
+
+                
 
 }
